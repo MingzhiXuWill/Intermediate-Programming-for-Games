71f70087191b9a8867bb887f794a8e9334a9ab88
Mingzhi Xu <mx2091@nyu.edu>
2023-03-29 09:21:04 -0400

    Week 09 Class

---
+++ Week 09/Assets/Script/GameManager.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using ChatGPTWrapper;
+using System;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    [SerializeField]
+    ChatGPTConversation chatGPT;
+
+    [SerializeField]
+    TMP_InputField iF_PlayerTalk;
+
+    [SerializeField]
+    TextMeshProUGUI tX_AIReply;
+
+    [SerializeField]
+    NPCController npcController;
+
+    string npcName = "Coco";
+    string playerName = "Player";
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        chatGPT.Init();
+    }
+
+    void Start()
+    {
+        // chatGPT.SendToChatGPT("{\"player_said\":" + "\"Hello! Who are you?\"}");
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonUp("Submit"))
+        {
+            SubmitChatMessage();
+        }
+    }
+
+    public void SubmitChatMessage()
+    {
+        if (iF_PlayerTalk.text != "")
+        {
+            chatGPT.SendToChatGPT("{\"player_said\":\"" + iF_PlayerTalk.text + "\"}");
+            ClearText();
+        }
+    }
+
+    void ClearText()
+    {
+        iF_PlayerTalk.text = "";
+    }
+
+    public void ReceiveChatGPTReply(string message)
+    {
+        print(message);
+
+        try
+        {
+            // Sometime the chatGPT
+            if (!message.EndsWith("}"))
+            {
+                if (message.Contains("}"))
+                {
+                    message = message.Substring(0, message.LastIndexOf("}") + 1);
+                }
+                else
+                {
+                    message += "}";
+                }
+            }
+            NpcJsonReceiver npcJSON = JsonUtility.FromJson<NpcJsonReceiver>(message);
+            string talkLine = npcJSON.reply_to_player;
+
+            tX_AIReply.text = "<color=#ff7082>" + npcName + ": </color>" + talkLine;
+
+            npcController.ShowAnimation(npcJSON.animation_name);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+            string talkLine = "Don't say that!";
+            tX_AIReply.text = "<color=#ff7082>" + npcName + ": </color>" + talkLine;
+        }
+    }
+}
